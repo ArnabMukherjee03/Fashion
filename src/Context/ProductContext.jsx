123d649d9dba5a3696163d2a4ec7e8d19125fbd5
Arnab Mukherjee <arnabmukherjee58260@gmail.com>
2023-05-09 02:03:38 +0530

    Contact Page Css Updated

--- src/Context/ProductContext.jsx
+++ src/Context/ProductContext.jsx
@@ -14,10 +14,9 @@ const ProductProvider = ({children})=>{
 
     useEffect(()=>{
         const fetchProducts = async () => {
-            window.scroll(0,0);
             const response = await fetch('https://fakestoreapi.com/products');
             const data = await response.json();
-            setLoading(true);
+            setLoading(true)
             setProducts(data);
         };
         fetchProducts();
